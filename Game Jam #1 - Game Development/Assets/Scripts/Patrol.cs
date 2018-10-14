39d76a750d83581e40bd102f430b9c7334799347
Geshh <gesha95_gesha95@abv.bg>
2018-10-14 18:13:40 +0300

    patrol script added

---
+++ Game Jam #1 - Game Development/Assets/Scripts/Patrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public float speed;
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform groundDetect;
+
+    private float timeBtwRotation = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetect.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+        {
+            if (timeBtwRotation > 0)
+            {
+                speed = 0;
+                timeBtwRotation -= Time.deltaTime;
+            }
+            else
+            {
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+                timeBtwRotation = 1f;
+                speed = 1f;
+            }
+        }
+    }
+}
