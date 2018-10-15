c1e1e477bb6fefbd66c6b163c022378cb003f6b3
Geshh <gesha95_gesha95@abv.bg>
2018-10-16 00:29:45 +0300

    Camera Zoom Out Animation Added

---
+++ Game Jam #1 - Game Development/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Animator anim;
+    private bool isZoomedOut = false;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.M) && isZoomedOut == false)
+            {
+                Debug.Log("Clicked" + isZoomedOut);
+                anim.SetTrigger("Active");
+                isZoomedOut = true;
+            }
+            else if (isZoomedOut == true)
+            {
+                Debug.Log("Clicked" + isZoomedOut);
+                anim.SetTrigger("Deactivate");
+                isZoomedOut = false;
+            }
+        }
+    }
+}
