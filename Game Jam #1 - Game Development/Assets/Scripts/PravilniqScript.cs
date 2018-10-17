3b389fd62f80cbaf5abece09aabf8a17291fd0b0
Geshh <gesha95_gesha95@abv.bg>
2018-10-17 22:48:26 +0300

    movement improved and added animations

--- Game Jam #1 - Game Development/Assets/Scripts/PravilniqScript.cs
+++ Game Jam #1 - Game Development/Assets/Scripts/PravilniqScript.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PravilniqScript : MonoBehaviour
 {
-
+    private Animator m_Anim;
     private Rigidbody2D rb;
     public float speed;
     public float jumpForce;
@@ -18,8 +18,6 @@ public class PlayerMovement : MonoBehaviour
     private float jumpTimeCounter;
     public float jumpTime;
     private bool isJumping;
-
-    private Animator anim;
     public bool m_FacingRight = true;
 
     public void Flip()
@@ -27,38 +25,22 @@ public class PlayerMovement : MonoBehaviour
         // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
         // Multiply the player's x local scale by -1.
-        Vector3 theScale = GameObject.FindGameObjectWithTag("Player").transform.localScale;
+        Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
     }
     void Start()
     {
-        anim = GetComponent<Animator>();
+        m_Anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        m_Anim.SetFloat("vSpeed", rb.velocity.y);
         moveInput = Input.GetAxisRaw("Horizontal");
-        if (moveInput == -1 && m_FacingRight)
-        {
-            Flip();
-        }
-        else if (moveInput == 1 && !m_FacingRight)
-        {
-            Flip();
-        }
-        if (moveInput != 0)
-        {
-            anim.SetBool("isWalking", true);
-            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-        }
-        else
-        {
-            anim.SetBool("isWalking", false);
-        }
-
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
     void Update()
@@ -67,6 +49,19 @@ public class PlayerMovement : MonoBehaviour
 
         if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
+            m_Anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+            if (rb.velocity.x > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (rb.velocity.x < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+
             isJumping = true;
             jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpForce;
